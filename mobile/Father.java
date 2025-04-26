0a6891e6f74de510001525b2579d3e7e3be60706
Cocotier785 <cocotier.desmottes@gmail.com>
2025-04-26 21:23:35 +0200

    Update Father.java

--- mobile/Father.java
+++ mobile/Father.java
@@ -3,11 +3,29 @@ package mobile;
 import map.Block;
 
 public class Father extends MobileElement {
-	
+	private boolean actionAnimal; // A mettre dans une classe individu intermédaire
+	private Animal target; // A mettre dans une classe individu intermédaire
 	private static final long serialVersionUID = 1L;
 	
 	public Father(Block position) {
 		this.position = position;
+		this.actionAnimal = false; // A mettre dans une classe individu intermédaire
+		this.target = null; // A mettre dans une classe individu intermédaire
 	}
+
+	//// A mettre dans une classe individu intermédaire : 
+	public boolean getActionAnimal(){
+		return actionAnimal;
+	}
+	public void setActionAnimal(boolean actionAnimal){
+		this.actionAnimal = actionAnimal;
+	}
+	public Animal getTarget(){
+		return target;
+	}
+	public void setTarget(Animal target){
+		this.target = target;
+	}
+	// ------------------
 }
 
