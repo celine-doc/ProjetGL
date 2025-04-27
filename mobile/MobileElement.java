6dc2294e1f592b976bb316835002c4866ad04d80
celine-doc <celinart.arkam@gmail.com>
2025-04-27 11:25:46 +0200

    Update MobileElement.java

--- mobile/MobileElement.java
+++ mobile/MobileElement.java
@@ -1,66 +1,78 @@
 package mobile;
 
-import java.io.Serializable;
 import java.util.ArrayList;
-
 import map.Block;
 
-public abstract class MobileElement implements Serializable{
+public class MobileElement {
+    protected ArrayList<String> listAction = new ArrayList<>();
+    protected Block position;
+    protected String location;
+    protected int actionTime;
+    protected long actionStartTime; // Temps de début de l'action
 
-	private String location;
-	private ArrayList<String> listAction = new ArrayList<String>();
-	private static final long serialVersionUID = 1L;
-	private Block position;
-	private boolean haveToMoove;
-	private int actionTime=0;
-	
-	public int getActionTime() {
-		return actionTime;
-	}
-	public void setActionTime(int actionTime) {
-		this.actionTime = actionTime;
-	}
-	
-	public boolean haveToMoove() {
-		return haveToMoove;
+    public String getAction() {
+        if (listAction.isEmpty()) {
+            return null;
+        }
+        return listAction.get(0);
+    }
+
+    public boolean dontHaveAction() {
+        return listAction.isEmpty();
+    }
+
+    public void addAction(String action) {
+        listAction.add(action);
+        actionStartTime = System.currentTimeMillis(); // Enregistrer le temps de début
+    }
+
+    public void supFirstAction() {
+        if (!listAction.isEmpty()) {
+            listAction.remove(0);
+        }
+    }
+
+    public Block getPosition() {
+        return position;
+    }
+
+    public void setPosition(Block position) {
+        this.position = position;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getActionTime() {
+        return actionTime;
+    }
+
+    public void setActionTime(int actionTime) {
+        this.actionTime = actionTime;
+    }
+
+    // Vérifier si l'action a dépassé la durée limite (en millisecondes)
+    public boolean isActionExpired(long duration) {
+        return System.currentTimeMillis() - actionStartTime > duration;
+    }
+
+	public MobileElement getTarget() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-	public void setHaveToMoove(boolean haveToMoove) {
-		this.haveToMoove = haveToMoove;
-	}
-	public boolean dontHaveAction() {
-		if(listAction.isEmpty()) {
-			return true;
-		}else {
-			return false;
-		}
-	}
-	public void setLocation(String newLocation) {
-		this.location = newLocation;
-	}
-	public String getLocation() {
-		return location;
-	}
-	
-	public void addAction(String newAction){
-		listAction.add(newAction);
-	}
-	public void supFirstAction() {
-		listAction.remove(0);
-	}
-	public String getAction() {
-		return listAction.get(0);
+	public void setActionAnimal(boolean b) {
+		// TODO Auto-generated method stub
 		
 	}
-	public ArrayList<String> getListAction(){
-		return listAction;
+
+	public void setTarget(Object object) {
+		// TODO Auto-generated method stub
+		
 	}
-	public Block getPosition() {
-		return position;
-	}
-	
-	public void setPosition(Block position) {
-		this.position = position;
-	}
-	
 }
